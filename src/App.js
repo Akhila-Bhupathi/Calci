6b95a6a953b33c0353ab2d42cd1551fc7d496d6a
Akhila-Bhupathi <akhi17cs@cmrit.ac.in>
2021-03-08 22:57:34 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ResultComponent from './components/Result';
+import KeyPadComponent from "./components/KeyPad";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            result: "0"
+        }
+    }
+
+    onClick = button => {
+
+        if(button === "="){
+            this.calculate()
+        }
+
+        else if(button === "C"){
+            this.reset()
+        }
+
+        else {
+            this.setState({
+                result: this.state.result + button
+            })
+        }
+    };
+
+
+    calculate = () => {
+        var Result = this.state.result
+
+        try {
+            this.setState({
+                result: (eval(Result) || "" ) + ""
+            })
+        } catch (e) {
+            this.setState({
+                result: "error"
+            })
+
+        }
+    };
+
+    reset = () => {
+        this.setState({
+            result: ""
+        })
+    };
+
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <h1>Hello world</h1>
+                    <h2>Hi</h2>
+                    <ResultComponent result={this.state.result}/>
+                    <KeyPadComponent onClick={this.onClick}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
